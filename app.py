ddf3fae0031a154dc36d238c1f50183ce7ee28da
Resx1983 <Moncadita1983@gmail.comgit config --global user.email you@example.comgit config --global user.email you@example.com  git config --global user.name Your>
2024-05-26 12:31:11 -0500

    Before Opinions

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("/home.html")
 
+@app.route("/comunidad")
+def comunidad():
+    return render_template("comunidad.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
